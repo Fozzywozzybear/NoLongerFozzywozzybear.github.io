c739c1ccc95387b2544e8a37d4a368e8c556f788
Fozzywozzybear <54587293+Fozzywozzybear@users.noreply.github.com>
2019-10-12 22:28:04 -0400

    Update to HW2.py
    Added changes to each class, to get total amount of money in each account if the holder= the self.__holder when teh account is made.

--- HW2.py
+++ HW2.py
@@ -15,66 +15,90 @@
             return "Your remaining balance is {}".format(self.balance)"""
 import random 
 from abc import *
-class BankOfAaron(ABC):
 
-    def __init__(self,person,id):
-        self.person = person 
-        self.id = random.randint(0,10)*7
-        
 class Account(ABC):
 
-    def __init__(self,person,holder,id):
+    def __init__(self,person,holder):
         self.person=person
+        self.holder=holder
         self.balance=0
-        self.__id=random.randint(0,10)*7
-        self.__holder=person
+    @abstractmethod
     def deposit(self,amount):
-        self.balance += amount
-        return 'your new balance is {}'.format(self.balance)
+        raise NotImplementedError
     @abstractmethod
     def withdraw(self):
         raise  NotImplementedError
 class CheckingAccount(Account):
 
-    def deposit(self,holder,id,amount):
+    def __init__(self,person,holder):
+        self.person=person
+        self.holder=holder
+        self.__holder=holder
+        self.balance=0
+        self.__id=self.__createID()
+        id=self.__id
+        
+
+
+    def deposit(self,holder,amount):
         if amount < 5:
             return "We don't except deposits under 5$"
         if amount >=5:
-            if holder == self.__holder:
                 self.balance+=amount
                 return 'your new balance is {}'.format(self.balance)
     def withdraw(self,holder,account,amount):#Checks if the person withdrawing is the user of their account
-        if holder == self.__holder:
             if amount <= self.balance:
                 self.balance -= amount
                 return "you have withdrawn {} your remaining balance is {}".format(amount,self.balance)
             if amount > self.balance:
                 return 'You do not have enough funds, your balance is {}'.format(self.balance)
-        else: 
-            return "the account you are accessing is not yours" 
-    def createID(self):
-        return random.randint(0,10)*7
+            else: 
+                return "the account you are accessing is not yours" 
+    def __createID(self):
+        return random.randint(1,10)*7
+    def getammount(self,holder):
+        if self.__holder==holder:
+            return self.balance
+        else:
+            return "you dont have access to this account"
+        return self.__id
+    def setID(self,holder,new_id):
+        if self.__holder==holder:
+            self.__id=new_id
+            return "your new id is {}".format(self.__id)
+        else:
+            return "you dont have access to this account"
+   
 class SavingAccount(Account):
+
+    def __init__(self,person,holder):
+        self.person=person
+        self.holder=holder
+        self.__holder=holder
+        self.balance=0
+        self.__id=self.__createID()
+        id=self.__id
+
     
     def deposit(self,holder,account,amount):
         if amount < 100:
             return "We don't except deposits under 100$"
         if amount >=5:
-            if holder == self.__holder:
                 self.balance+=amount
                 return 'your new balance is {}'.format(self.balance)
     def withdraw(self,holder,account,amount):
         if amount < 1000:
             return 'you deposit {} more $ before you are able to withdraw'.format(1000-self.balance)
-class ChangeId(Account):
-
+    def __createID(self):
+        return random.randint(1,10)*7
     def getid(self):
         return self.__id
     def setID(self,holder,new_id):
-        if holder == self.__holder:
+        if self.__holder==holder:
             self.__id=new_id
+            return "your new id is {}".format(self.__id)
         else:
-            return "you do not have access to change the id of this account"
+            return "you don't have access to this account"
 
         
 
