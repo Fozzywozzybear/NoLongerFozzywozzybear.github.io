d10cecea5847ac00d7acef468dcb62f5c3283961
Fozzywozzybear <54587293+Fozzywozzybear@users.noreply.github.com>
2019-10-17 15:55:26 -0400

    Reeee

--- LAB7.py
+++ LAB7.py
@@ -62,27 +62,29 @@ class OrderedLinkedList:
 
     def add(self, value):
         if isinstance(value,float) or isinstance(value,int):
-           new_node=Node(value)
-           current=self.head
-           if self.isEmpty():
-               self.head=new_node
-               self.tail=new_node
-           if new_node.value <= self.head.value:
-                new_node.next=current
-                new_node=self.head
-           elif new_node.value<=current.value:
-                new_node=self.head
-                current.next=current
-           else:
+            new_node=Node(value)
+            current=self.head
+            if self.isEmpty():
+                self.head=new_node
+                self.tail=new_node
+                return 
+            if new_node.value > self.tail.value:
+                current.next=new_node
+                self.tail=new_node
+                return 
+            if new_node.value < self.head.value
+                current=new_node
+                self.head=new_node
+                return 
+            else:
                 while current.next!=None:
-                    if current.next > new_node:
+                    if current.next.value >= new_node.value:
                         current.next=new_node
+                        self.tail=new_node
                         break
-                self.tail =new_node
-                current=current.next
-
-
-
+                current.next=new_node
+                self.tail=new_node
+                return 
     def pop(self):
         current=self.head
         while current!=None:
@@ -95,6 +97,7 @@ class OrderedLinkedList:
 
 
     def remDuplicates(self):
+        pass
         current=self.head
         while current.next!=None:
             if current==current.next:
